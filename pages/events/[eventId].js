0febb1897a906a0dd26ff136f363a4e85f1a66e4
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2022-01-05 11:59:07 +0530

    dynamic page handling
    completed statically generating dynamic page on server side.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,40 +1,55 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-utils';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
+    const event = props.event;
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+    if (!event) {
+        return (
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        );
+    }
 
-  if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
-  }
+}
 
-  return (
-    <Fragment>
-      <EventSummary title={event.title} />
-      <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
-      />
-      <EventContent>
-        <p>{event.description}</p>
-      </EventContent>
-    </Fragment>
-  );
+export async function getStaticProps(context) {
+    const eventId = context.params.eventId;
+    const event = await getEventById(eventId);
+    return {
+        props: {
+            event,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const events = await getAllEvents();
+    const paths = events.map((event) => ({ params: { eventId: event.id } }));
+    return {
+        paths: paths,
+        fallback: false,
+    };
 }
 
 export default EventDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-utils';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+    return (
+        <div>
+            <EventList items={props.events} />
+        </div>
+    );
+}
 
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+export async function getStaticProps() {
+    const featuredEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events: featuredEvents,
+        },
+    };
 }
 
 export default HomePage;
