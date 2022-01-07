d2ce5052b86b5faf1ff18906b0f91a1b2a397d81
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2022-01-08 02:17:02 +0530

    Optimizing next app - Head component
    added title and meta to homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import { getFeaturedEvents } from '../helpers/api-utils';
 import EventList from '../components/events/event-list';
 
 function HomePage(props) {
     return (
         <div>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="Events app for Rishikesh!" />
+            </Head>
             <EventList items={props.events} />
         </div>
     );
