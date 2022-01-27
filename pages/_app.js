ab4fb895459d7de8dcb928c13a315f6fcd4428dc
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2022-01-28 01:53:35 +0530

    added _document.js file
    added _document.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale="
+                />
+                <title>Events app</title>
+            </Head>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,6 +2,7 @@
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import Head from 'next/head';
 
 import { getFilteredEvents } from '../../helpers/api-utils';
 import EventList from '../../components/events/event-list';
@@ -33,9 +34,20 @@ function FilteredEventsPage(props) {
             setAllEvents(events);
         }
     }, [data]);
+    let pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta name="description" content={`A list of filtered events!`} />
+        </Head>
+    );
 
     if (!allEvents) {
-        return <p className="center">Loading...</p>;
+        return (
+            <Fragment>
+                {pageHeadData}
+                <p className="center">Loading...</p>
+            </Fragment>
+        );
     }
 
     const filteredYear = filterData[0];
@@ -43,6 +55,16 @@ function FilteredEventsPage(props) {
 
     const numYear = +filteredYear;
     const numMonth = +filteredMonth;
+
+    pageHeadData = (
+        <Head>
+            <title>Filtered Events</title>
+            <meta
+                name="description"
+                content={`All events for ${numMonth}/${numYear}`}
+            />
+        </Head>
+    );
 
     if (
         isNaN(numYear) ||
@@ -54,6 +76,8 @@ function FilteredEventsPage(props) {
     ) {
         return (
             <Fragment>
+                {pageHeadData}
+
                 <ErrorAlert>
                     <p>Invalid filter. Please adjust your values!</p>
                 </ErrorAlert>
@@ -83,6 +107,8 @@ function FilteredEventsPage(props) {
     ) {
         return (
             <Fragment>
+                {pageHeadData}
+
                 <ErrorAlert>
                     <p>Invalid filter. Please adjust your values!</p>
                 </ErrorAlert>
@@ -96,6 +122,8 @@ function FilteredEventsPage(props) {
     if (!filteredEvents || filteredEvents.length === 0) {
         return (
             <Fragment>
+                {pageHeadData}
+
                 <ErrorAlert>
                     <p>No events found for the chosen filter!</p>
                 </ErrorAlert>
@@ -110,6 +138,8 @@ function FilteredEventsPage(props) {
 
     return (
         <Fragment>
+            {pageHeadData}
+
             <ResultsTitle date={date} />
             <EventList items={filteredEvents} />
         </Fragment>
